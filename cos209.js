8c0dd69801958bed9f2b2c3f09a9efde66278780
Andrea Andy <129201824+EbosetalehAndrea@users.noreply.github.com>
2024-12-04 23:33:41 +0100

    Add files via upload
    Added COS209 JavaScript file for assignment submission on pushing assignment to github.

---
+++ cos209.js
@@ -0,0 +1,23 @@
+const details = {
+name: "Andrea",
+age: 17,
+contact: {
+    "phone number": "0811111",
+    email: "ebosetaleh@yahoo.com",
+},
+"birth details": {
+    date: "28/04/2007",
+    state: "lagos",
+    "place of origin": "Edo",
+    country: "Nigeria",
+},
+address: {
+    street: "16, Morgan estate",
+    town: "Ikeja",
+    state: "lagos",
+    country: "Nigeria",
+}
+
+};
+
+console.log (details);
